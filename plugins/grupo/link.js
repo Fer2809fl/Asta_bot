8ea8fd0629c097d62427cab6051514db9197107f
Víctor Manuel | Orion's Wolf <vm1008079@gmail.com>
2026-03-21 14:29:17 -0400

    Update link.js

--- plugins/grupo/link.js
+++ plugins/grupo/link.js
@@ -1,9 +1,17 @@
 var handler = async (m, { conn, args }) => {
-let group = m.chat
-const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-let message = `\t*⌁☍꒷₊˚ Link del Grupo ꒷₊˚⌁☍*\n\n> \`Link:\` ${link}`
-await conn.sendMessage(group, { image: { url: pp }, caption: message })
+    let group = m.chat
+    const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
+    let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+    
+    let message = 
+        `> . ﹡ ﹟ 🔗 ׄ ⬭ *ʟɪɴᴋ ᴅᴇʟ ɢʀᴜᴘᴏ*\n\n` +
+        `*ㅤꨶ〆⁾ ㅤׄㅤ⸼ㅤׄ 📨 ㅤ֢ㅤ⸱ㅤᯭִ*\n` +
+        `ׅㅤ𓏸𓈒ㅤׄ *ɴᴏᴍʙʀᴇ* :: ${await conn.getName(group)}\n` +
+        `ׅㅤ𓏸𓈒ㅤׄ *ᴄᴏ́ᴅɪɢᴏ* :: ${link.split('/').pop()}\n\n` +
+        `> ✦ *ʟɪɴᴋ* :: ${link}\n\n` +
+        `> ✧ *ɴᴏᴛᴀ* :: Usa este enlace para invitar a nuevos miembros`
+
+    await conn.sendMessage(group, { image: { url: pp }, caption: message })
 }
 
 handler.help = ['link']
