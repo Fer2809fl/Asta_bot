f90d509deed8e30e46cfaef6d017ea283edc4931
Víctor Manuel | Orion's Wolf <vm1008079@gmail.com>
2026-03-21 14:12:45 -0400

    Update admins.js

--- plugins/grupo/admins.js
+++ plugins/grupo/admins.js
@@ -1,18 +1,22 @@
-const handler = async (m, {conn, participants, groupMetadata, args}) => {
-const primaryBot = global.db.data.chats[m.chat].primaryBot
-if (primaryBot && conn.user.jid !== primaryBot) throw !1
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
-const groupAdmins = participants.filter((p) => p.admin)
-const listAdmin = groupAdmins.map(v => `● @${v.id.split('@')[0]}`).join('\n')
-const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
-const pesan = args.join` `
-const oi = `» ${pesan}`
-const text = `『✦』Admins del grupo:  
-  
-${listAdmin}
+const handler = async (m, { conn, participants, groupMetadata, args }) => {
+    const primaryBot = global.db.data.chats[m.chat].primaryBot
+    if (primaryBot && conn.user.jid !== primaryBot) throw !1
+    
+    const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
+    const groupAdmins = participants.filter((p) => p.admin)
+    const listAdmin = groupAdmins.map(v => `ׅㅤ𓏸𓈒ㅤׄ 👤 @${v.id.split('@')[0]}`).join('\n')
+    const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+    const pesan = args.join` `
+    
+    const text = 
+        `> . ﹡ ﹟ 🛡️ ׄ ⬭ *ᴀᴅᴍɪɴɪsᴛʀᴀᴅᴏʀᴇs*\n\n` +
+        `*ㅤꨶ〆⁾ ㅤׄㅤ⸼ㅤׄ ⚡ ㅤ֢ㅤ⸱ㅤᯭִ*\n` +
+        `${listAdmin}\n\n` +
+        `> ✦ *ᴍᴇɴsᴀᴊᴇ* :: ${pesan || 'Sin especificar'}`
 
-❍ Mensaje ${oi || 'Sin especificar'}`
-conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]})
+    conn.sendFile(m.chat, pp, 'admins.jpg', text, m, false, { 
+        mentions: [...groupAdmins.map((v) => v.id), owner] 
+    })
 }
 
 handler.help = ['admins']
